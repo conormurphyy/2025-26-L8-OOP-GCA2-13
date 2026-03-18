b08cecbca98e1412171341b441183d1a112d9f30
cmurphy-06 <D00279403@student.dkit.ie>
2026-03-18 15:42:57 +0000

    JSON request

--- src/main/java/org/example/recipe/Request.java
+++ src/main/java/org/example/recipe/Request.java
@@ -1,4 +1,27 @@
 package org.example.recipe;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class Request {
+
+    private String   type;
+    private JsonNode payload;
+
+    public Request() {
+        type    = "";
+        payload = null;
+    }
+
+    public Request(String type, JsonNode payload) {
+        this.type    = type;
+        this.payload = payload;
+    }
+
+    public String getType() { return type; }
+
+    public void setType(String type) { type = type; }
+
+    public JsonNode getPayload() { return payload; }
+
+    public void setPayload(JsonNode payload) { payload = payload; }
 }
