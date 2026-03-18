ce24e5ad14a5090c9a1253976126121cd1008082
cmurphy-06 <D00279403@student.dkit.ie>
2026-03-18 15:38:20 +0000

    JSON response

---
+++ src/main/java/org/example/recipe/Request.java
@@ -0,0 +1,4 @@
+package org.example.recipe;
+
+public class Request {
+}
---
+++ src/main/java/org/example/recipe/Response.java
@@ -0,0 +1,53 @@
+package org.example.recipe;
+
+public class Response<T> {
+
+    private String status;
+    private String message;
+    private T data;
+
+    public Response() {
+        status = "";
+        message = "";
+        data = null;
+    }
+
+    public Response(String status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    // HELPERS:
+    public static <T> Response<T> success(String message, T data) {
+        return new Response<>("Success...", message, data);
+    }
+
+    public static <T> Response<T> failure(String message) {
+        return new Response<>("Failure...", message, null);
+    }
+}
