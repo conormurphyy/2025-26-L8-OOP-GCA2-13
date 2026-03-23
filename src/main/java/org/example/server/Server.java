2bbedb8fb2466b914a68e071ea55b8a409c4d817
conor <conormccracken1@icloud.com>
2026-03-23 21:44:51 +0000

    implementation furthered

--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -4,15 +4,28 @@ import com.fasterxml.jackson.annotation.JsonTypeId;
 import org.example.dao.IngredientDao;
 import org.example.dao.RecipeDao;
 import org.example.dao.UserDao;
+import org.example.dao.jdbc.JDBCIngredientDao;
+import org.example.dao.jdbc.JDBCRecipeDao;
+import org.example.dao.jdbc.JdbcUserDao;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class Server {
+    private final int _port;
+    private final UserDao _userDao;
+    private final RecipeDao _recipeDao;
+    private final IngredientDao _ingredientDao;
+    private final ExecutorService _pool;
     public Server(int port, UserDao userDao, RecipeDao recipeDao, IngredientDao ingredientDao)
+
     {
+
         if(port <1_024 || port > 65_535)
         {
             throw new IllegalArgumentException("Port must be between 1024 and 65535");
@@ -34,6 +47,7 @@ public class Server {
         _userDao = userDao;
         _recipeDao = recipeDao;
         _ingredientDao = ingredientDao;
+        _pool = Executors.newCachedThreadPool();
     }
 
     public void start() throws IOException {
@@ -56,6 +70,32 @@ public class Server {
     }
     private void stop()
     {
+        System.out.println("Stopping RecipeHub server");
+        _pool.shutdown();
+        try{
+            if(!_pool.awaitTermination(5, TimeUnit.SECONDS))
+            {
+                System.out.println("Server did not stop gracefully");
+                _pool.shutdownNow();
+            }
+        }
+        catch (InterruptedException e)
+        {
+            _pool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("RecipeHub server stopped");
+    }
 
+    public static void main(String[] args) throws IOException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/recipehub";
+        String user = "admin";
+        String pass = "";
+
+        UserDao userDao = new JdbcUserDao(url, user, pass);
+        RecipeDao recipeDao = new JDBCRecipeDao(url, user, pass);
+        IngredientDao ingredientDao = new JDBCIngredientDao(url, user, pass);
+
+        new Server(8080, userDao, recipeDao, ingredientDao).start();
     }
 }
