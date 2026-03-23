85a2ca5e04810d88bdc526bcd6836fe1b0900c42
cmurphy-06 <D00279403@student.dkit.ie>
2026-03-23 18:49:29 +0000

    Added main App
    This is where the application will be ran

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class App {
+    public static void run() {
+        new App().start();
+    }
+
+    private void start() {
+        //  Main App
+    }
+
+}
