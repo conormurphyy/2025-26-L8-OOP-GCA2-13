187e4250cd6565a78dafe9cc3a24969bcf772bd9
conor <conormccracken1@icloud.com>
2026-03-23 22:11:40 +0000

    some work on ClientHandler

--- src/main/java/org/example/server/ClientHandler.java
+++ src/main/java/org/example/server/ClientHandler.java
@@ -6,10 +6,17 @@ import org.example.dao.RecipeDao;
 import org.example.dao.UserDao;
 import org.example.shared.ClientRequest;
 import org.example.shared.ServerResponse;
+import org.example.util.JsonUtil;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+
+import static sun.misc.Signal.handle;
 
 public class ClientHandler implements Runnable{
     @FunctionalInterface
@@ -47,9 +54,38 @@ public class ClientHandler implements Runnable{
         _userDao=userDao;
         _recipeDao=recipeDao;
         _ingredientDao=ingredientDao;
-        _mapper=JsonUtil;
+        _mapper= JsonUtil.getMapper();
         _handlers=new HashMap<>();
         _running=true;
-
+        registerHandlers();
     }
+    @Override
+    public void run(){
+String threadName = Thread.currentThread().getName();
+        System.out.println("ClientHandler thread started: " + threadName + " for client: " + _socket.getInetAddress().getHostAddress());
+
+        try(BufferedReader in = new BufferedReader(new InputStreamReader(_socket.getInputStream()));
+            PrintWriter out = new PrintWriter(_socket.getOutputStream(), true))
+        {
+            String line;
+            while(_running && (line = in.readLine())!=null)
+            {
+                String response = handle(line);
+                out.println(response);
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("Client disconnected: " + e.getMessage());
+        }
+        finally {
+            try{
+                _socket.close();
+            } catch(IOException ignored){
+
+            }
+            System.out.println(threadName + " thread stopped");
+        }
+    }
+
 }
