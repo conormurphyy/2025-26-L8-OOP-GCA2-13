bf91fb869996edcd359a32a0024c03c786242208
cmurphy-06 <D00279403@student.dkit.ie>
2026-03-24 17:55:54 +0000

    Added equals and hash to ingredient and addded comments

--- src/main/java/org/example/domain/Ingredient.java
+++ src/main/java/org/example/domain/Ingredient.java
@@ -1,98 +1,128 @@
 package org.example.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Objects;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Ingredient {
 
-    private int ingredientId;
-    private String name;
-    private double calories;
-    private double protein;
-    private double carbs;
-    private double fat;
+    //Fields
+    private int fIngredientId;
+    private String fName;
+    private double fCalories;
+    private double fProtein;
+    private double fCarbs;
+    private double fFat;
 
-    public Ingredient(int ingredientId, String name, double calories) {
-
-        if (ingredientId < 0)
-            throw new IllegalArgumentException("Ingredient ID cannot be less than zero");
-        if (name == null)
-            throw new IllegalArgumentException("Ingredient name cannot be null");
-        if (calories < 0) {
-            throw new IllegalArgumentException("Calories cannot be less than zero");
-        }
-
-        this.ingredientId = ingredientId;
-        this.name = name;
-        this.calories = calories;
+    //Constructors
+    public Ingredient() {
+        fIngredientId = 0;
+        fName = "";
+        fCalories = 0.0;
+        fProtein = 0.0;
+        fCarbs = 0.0;
+        fFat = 0.0;
     }
 
     public Ingredient(int ingredientId, String name, double calories, double protein, double carbs, double fat) {
-        this.ingredientId = ingredientId;
-        this.name = name;
-        this.calories = calories;
-        this.protein = protein;
-        this.carbs = carbs;
-        this.fat = fat;
+        if (ingredientId < 0) {
+            throw new IllegalArgumentException("ingredientId must be >= 0");
+        }
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("name is required");
+        }
+        if (calories < 0.0 || protein < 0.0 || carbs < 0.0 || fat < 0.0) {
+            throw new IllegalArgumentException("macros must be >= 0");
+        }
 
-
+        fIngredientId = ingredientId;
+        fName = name.trim();
+        fCalories = calories;
+        fProtein = protein;
+        fCarbs = carbs;
+        fFat = fat;
     }
 
+    //Public API
     public int getIngredientId() {
-        return ingredientId;
+        return fIngredientId;
     }
 
     public void setIngredientId(int ingredientId) {
-        this.ingredientId = ingredientId;
+        fIngredientId = ingredientId;
     }
 
     public String getName() {
-        return name;
+        return fName;
     }
 
     public void setName(String name) {
-        this.name = name;
+        fName = name;
     }
 
     public double getCalories() {
-        return calories;
+        return fCalories;
     }
 
     public void setCalories(double calories) {
-        this.calories = calories;
+        fCalories = calories;
     }
 
     public double getProtein() {
-        return protein;
+        return fProtein;
     }
 
     public void setProtein(double protein) {
-        this.protein = protein;
+        fProtein = protein;
     }
 
     public double getCarbs() {
-        return carbs;
+        return fCarbs;
     }
 
     public void setCarbs(double carbs) {
-        this.carbs = carbs;
+        fCarbs = carbs;
     }
 
     public double getFat() {
-        return fat;
+        return fFat;
     }
 
     public void setFat(double fat) {
-        this.fat = fat;
+        fFat = fat;
     }
 
+    //Helpers
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fIngredientId, fName, fCalories, fProtein, fCarbs, fFat);
+    }
 
     @Override
     public String toString() {
-        return "Ingredient{" +
-                "ingredientId=" + ingredientId +
-                ", name='" + name + '\'' +
-                ", calories=" + calories +
-                ", protein=" + protein +
-                ", carbs=" + carbs +
-                ", fat=" + fat +
-                '}';
+        return "Ingredient{id=" + fIngredientId
+                + ", name='" + fName + '\''
+                + ", calories=" + fCalories
+                + ", protein=" + fProtein
+                + ", carbs=" + fCarbs
+                + ", fat=" + fFat + "}";
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Ingredient)) {
+            return false;
+        }
+        Ingredient other = (Ingredient) o;
+        return fIngredientId == other.fIngredientId
+                && Double.compare(fCalories, other.fCalories) == 0
+                && Double.compare(fProtein, other.fProtein) == 0
+                && Double.compare(fCarbs, other.fCarbs) == 0
+                && Double.compare(fFat, other.fFat) == 0
+                && Objects.equals(fName, other.fName);
     }
 }
