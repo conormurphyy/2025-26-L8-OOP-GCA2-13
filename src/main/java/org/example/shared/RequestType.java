21ab48ad317ed7fba787eb0ba2bff19f241bc2f9
cmurphy-06 <D00279403@student.dkit.ie>
2026-03-24 18:59:19 +0000

    RequestType Comments

--- src/main/java/org/example/shared/RequestType.java
+++ src/main/java/org/example/shared/RequestType.java
@@ -1,20 +1,21 @@
 package org.example.shared;
 
 public enum RequestType {
+    //User
     USER_GET_ALL,
     USER_GET_BY_ID,
     USER_INSERT,
     USER_UPDATE,
     USER_DELETE,
     USER_FILTER,
-
+    //Recipe
     RECIPE_GET_ALL,
     RECIPE_GET_BY_ID,
     RECIPE_INSERT,
     RECIPE_UPDATE,
     RECIPE_DELETE,
     RECIPE_FILTER,
-
+    //Ingredient
     INGREDIENT_GET_ALL,
     INGREDIENT_GET_BY_ID,
     INGREDIENT_INSERT,
