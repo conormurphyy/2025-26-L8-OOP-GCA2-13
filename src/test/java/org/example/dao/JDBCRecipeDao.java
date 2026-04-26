88921c5546ed5aaf5c97883c8969dc74c32a905f
conor <conormccracken1@icloud.com>
2026-04-26 20:47:54 +0100

    some tests

--- src/test/java/org/example/dao/JDBCRecipeDao.java
+++ src/test/java/org/example/dao/JDBCRecipeDao.java
@@ -1,9 +1,11 @@
 package org.example.dao;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.dao.jdbc.JDBCRecipeDao;
+import org.example.domain.Recipe;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class jdbcRecipeDao {
 
@@ -16,5 +18,31 @@ class jdbcRecipeDao {
         );
 
         assertNotNull(dao.getAllRecipes()); // basic read test
+    }
+    @Test
+    void insertRecipeWithImageReturnGenID() throws Exception {
+        JDBCRecipeDao dao = new JDBCRecipeDao(
+                "jdbc:mysql://localhost:3306/recipehub",
+                "root",
+                ""
+        );
+        byte[] img = "x".getBytes();
+
+        Recipe r1 = new Recipe(0,1,"Test",1,"d",100,true,img,"x.txt","text/plain",1000);
+        int id = dao.insertRecipeWithImage(r1);
+        System.out.println(id);
+        assertTrue(id>0);
+        //each time it runs it creates a new recipe in the database and the id increases by 1
+
+    }
+    @Test
+    void recipeJsonRoundTrip() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+
+        Recipe r = new Recipe(1,2,"Test",1,"d",100,true);
+        String json = mapper.writeValueAsString(r);//serialize to json
+        Recipe r2 = mapper.readValue(json, Recipe.class);//deserialize from json
+
+        assertEquals(r,r2);
     }
 }
