558e794f1b6d680514a3f7cf8291196dccc589ae
Richie <d00280247@student.dkit.ie>
2026-05-08 02:37:24 +0100

    ClientRequest test

--- src/test/java/org/example/shared/ClientRequestTest.java
+++ src/test/java/org/example/shared/ClientRequestTest.java
@@ -1,7 +1,63 @@
 package org.example.shared;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClientRequestTest {
+
+    @Test
+    void emptyConstructorSetsDefaults() {
+        ClientRequest cr = new ClientRequest();
+        assertEquals("", cr.getType());
+        assertNull(cr.getPayload());
+    }
+
+    @Test
+    void constructorWithTypeAndPayload() {
+        ClientRequest cr = new ClientRequest("USER_INSERT", null);
+        assertEquals("USER_INSERT", cr.getType());
+        assertNull(cr.getPayload());
+    }
+
+    @Test
+    void setTypeAndGet() {
+        ClientRequest cr = new ClientRequest();
+        cr.setType("RECIPE_DELETE");
+        assertEquals("RECIPE_DELETE", cr.getType());
+    }
+
+    @Test
+    void setTypeWithNullDefaultsToEmpty() {
+        ClientRequest cr = new ClientRequest();
+        cr.setType(null);
+        assertEquals("", cr.getType());
+    }
+
+    @Test
+    void getIntReturnsNegativeOneWhenPayloadIsNull() {
+        ClientRequest cr = new ClientRequest();
+        assertEquals(-1, cr.getInt("someKey"));
+    }
+
+    @Test
+    void getDoubleReturnsNegativeOneWhenPayloadIsNull() {
+        ClientRequest cr = new ClientRequest();
+        assertEquals(-1, cr.getDouble("someKey"));
+    }
+
+    @Test
+    void getBooleanReturnsFalseWhenPayloadIsNull() {
+        ClientRequest cr = new ClientRequest();
+        assertFalse(cr.getBoolean("someKey"));
+    }
+
+    @Test
+    void getStringReturnsEmptyWhenPayloadIsNull() {
+        ClientRequest cr = new ClientRequest();
+        assertEquals("", cr.getString("someKey"));
+    }
+
+    // This test was written using chatgpt
 
 }
